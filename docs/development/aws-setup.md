ccb7648fa255cb93295bb1d8981f885e10c745b3
Hozay <josmchavz@gmail.com>
2024-11-09 13:36:10 -0800

    added a few empty docs

---
+++ docs/development/aws-setup.md
@@ -0,0 +1,11 @@
+
+:::info Fedora Workstation was used for this guide
+
+# Setting up AWS Amplify
+
+This guide will be exactly similar to [Amplify's Setup Guide](https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates).
+
+Even though the project already has Amplify installed within its packages, AWS requires 
+for every developer to login into the AWS account. This guide will help you
+in figuring out how to do that.
+
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ucsc-slugpath.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ucsc-slugpath', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
